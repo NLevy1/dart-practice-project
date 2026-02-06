29433d8dbad392221c1f3deea50e8cbc81e3eb45
NLevy1 <nathanaellevy@hotmail.com>
2026-02-06 18:10:48 +0100

    Add argparse

--- bin/unit_converter_practice.dart
+++ bin/unit_converter_practice.dart
@@ -1,1 +1,12 @@
+import 'package:dart_practice_projects/unit_converter_functions.dart';
+import 'package:args/args.dart';
 
+void main (List<String> arguments) {
+    final parser = ArgParser()
+    ..addOption('celsius', abbr: 'c', mandatory: true);
+    final results = parser.parse(arguments);
+    String celsius_parsed = results['celsius'];
+    final celsius_double = double.parse(celsius_parsed);
+
+    print(convertCelsiusToFahrenheit(celsius_double));
+}
